cc3b9d08a7b1e29428849a0ac4e158b5f1decfbb
prashant2150487 <you@example.com>
2022-08-22 19:27:42 +0530

    Updates

---
+++ precache-manifest.66629a3b4c88b820f71c1c402369c7d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d14e37ebfa8c63977a",
+    "url": "/reminder/static/css/main.bf56839c.chunk.css"
+  },
+  {
+    "revision": "a8d14e37ebfa8c63977a",
+    "url": "/reminder/static/js/main.a8d14e37.chunk.js"
+  },
+  {
+    "revision": "24758a8b680d33a928be",
+    "url": "/reminder/static/js/1.24758a8b.chunk.js"
+  },
+  {
+    "revision": "a0c1f8d1e35cfc77d4c2",
+    "url": "/reminder/static/js/runtime~main.a0c1f8d1.js"
+  },
+  {
+    "revision": "97747f8e0baaf4946b95979a060c1871",
+    "url": "/reminder/index.html"
+  }
+];
